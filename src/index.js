ae4f026de60ae2823cae34b53b5a034488b10944
qswaquang <qswaquang@gmail.com>
2022-10-10 01:46:42 +0700

    port

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ app.use('/api/user', userRoute);
 app.use('/api/messages', messageRoute);
 app.use('/api/conversation', conversationRoute);
 
-server.listen(process.env.PORT, () => {
+server.listen(process.env.PORT || 3920, () => {
   console.log(`Express running => PORT ${server.address().port}`);
   connectDatabase();
 });
