adb53823f67db38d760a36c058468b401839a631
qswaquang <qswaquang@gmail.com>
2022-10-10 11:18:48 +0700

    fix pro

--- src/models/Conversation.js
+++ src/models/Conversation.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const ConversationSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   sender: { type: String, require: true, ref: "User" },
-  between: { type: [String] },
+  between: { type: [{ user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, nick_name: String }] },
   room_chat: { type: String, ref: "RoomChat" },
   last_messages: { type: mongoose.Schema.Types.ObjectId, require: true, ref: "Message" },
-  nick_name: { type: String },
   seen_last_messages: { type: Boolean, require: true, default: false }
 }, { timestamps: true })
 
--- src/models/RoomChat.js
+++ src/models/RoomChat.js
@@ -4,8 +4,6 @@ const RoomChatSchema = new Schema({
   _id: { type: String },
   name: { type: String, },
   avatar: { type: String },
-  list_user: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  last_message: { type: { content: String, time: Date, isSeen: Boolean } }
 }, { timestamps: true })
 
 module.exports = model("RoomChat", RoomChatSchema);
