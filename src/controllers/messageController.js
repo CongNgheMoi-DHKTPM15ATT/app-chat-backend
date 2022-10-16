9f1b933ec3ae3b4526051a091fe5de9178653710
hainamdev <hainamdev@gmail.com>
2022-10-17 02:24:14 +0700

    fix messageController

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,45 +1,48 @@
-const asyncHandler = require('express-async-handler');
-const Message = require('./../models/Message.js');
-const Conversation = require('./../models/Conversation.js');
-const User = require('./../models/User.js');
-const { ObjectId } = require('mongoose')
+const asyncHandler = require("express-async-handler");
+const Message = require("./../models/Message.js");
+const Conversation = require("./../models/Conversation.js");
+const User = require("./../models/User.js");
+const { ObjectId } = require("mongoose");
 
 const messageController = {
   getMessageByConversation: asyncHandler(async (req, res, next) => {
     const { conversation_id } = req.body;
-    const messages_document = await Message.find({ conversation: conversation_id }).populate({
-      path: 'conversation'
-    }).populate('sender')
+    console.log("conver: " + conversation_id);
+    const messages_document = await Message.find({
+      conversation: conversation_id,
+    })
+      .populate({
+        path: "conversation",
+      })
+      .populate("sender");
 
-    const messages = []
+    const messages = [];
     let sender;
 
     messages_document.forEach((message) => {
-
       message.conversation.members.forEach((member) => {
         if (member.user_id.toString() === message.sender._id.toString()) {
           sender = {
-            'user_id': member.user_id,
-            'nick_name': member.nick_name
-          }
+            user_id: member.user_id,
+            nick_name: member.nick_name,
+          };
           return;
         }
-      })
+      });
       messages.push({
-        "_id": message._id,
-        "content": message.content,
-        "content_type": message.content_type,
-        "deleted": false,
-        "sender": sender,
-        "createdAt": message.createAt,
-        "updatedAt": message.updateAt,
-      })
-    })
-
-
+        _id: message._id,
+        content: message.content,
+        content_type: message.content_type,
+        deleted: false,
+        sender: sender,
+        createdAt: message.createdAt,
+        updatedAt: message.updatedAt,
+      });
+    });
+    messages.reverse();
     return res.json({
-      messages
-    })
+      messages,
+    });
   }),
   // getLastMessage: asyncHandler(async (req, res, next) => {
   //   const { senderId, receiverId } = req.query;
@@ -48,7 +51,7 @@ const messageController = {
   // }),
   save: asyncHandler(async (req, res, next) => {
     try {
-      console.log('start save msg')
+      console.log("start save msg");
       const { sender_id, conversation_id, text } = req.body;
 
       const message = await new Message({
@@ -57,19 +60,27 @@ const messageController = {
         conversation: conversation_id,
       }).save();
 
-      console.log(conversation_id)
+      console.log(conversation_id);
 
-      const conversation = await Conversation.findByIdAndUpdate({ _id: conversation_id }, {
-        last_message: message._id,
-      });
+      const conversation = await Conversation.findByIdAndUpdate(
+        { _id: conversation_id },
+        {
+          last_message: message._id,
+        }
+      );
 
-      if (message) return res.status(200).json({ message: 'Message added successfully.', 'data': message });
-      else return res.status(400).json({ message: 'Failed to add message to the database' });
+      if (message)
+        return res
+          .status(200)
+          .json({ message: "Message added successfully.", data: message });
+      else
+        return res
+          .status(400)
+          .json({ message: "Failed to add message to the database" });
     } catch (ex) {
       next(ex);
     }
-
   }),
-}
+};
 
 module.exports = messageController;
