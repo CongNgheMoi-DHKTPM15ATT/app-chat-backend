a9eea26e6676c85c8396ef71719e537eb8b4512a
hainamdev <hainamdev@gmail.com>
2022-10-24 15:20:34 +0700

    fix socket

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -86,49 +86,81 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on("request_video_call", (data) => {
-    console.log("step 1");
-    const { senderId, receiverId, sender_name, receiver_name } = data;
-    //  if (!(_userCall.get(senderId) && _userCall.get(receiverId))) {
-    const socketId = _userOnlines.get(receiverId);
-    socket.to(socketId).emit("request_video_call", {
-      senderId: senderId,
-      sender_name: sender_name,
-      receiver_name: receiver_name,
-      receiverId: receiverId,
-    });
-    console.log("step 2");
-    socket.on("callUser", (signal) => {
-      console.log("step 3");
-      socket.to(socketId).emit("callUser", {
-        signal: signal,
-        senderId: senderId,
-        sender_name: sender_name,
-        receiver_name: receiver_name,
-        receiverId: receiverId,
-      });
-      console.log("step 4");
-    });
-    //    }
-  });
+  // socket.on("callUser", (data) => {
+  //   const { senderId, receiverId, sender_name, receiver_name, signalData } =
+  //     data;
+  //   const socketId = _userOnlines.get(receiverId);
+  //   console.log("step 1");
+  //   io.to(socketId).emit("request_video_call", {
+  //     senderId: senderId,
+  //     sender_name: sender_name,
+  //     receiver_name: receiver_name,
+  //     receiverId: receiverId,
+  //   });
+  //   console.log("step 2");
+  //   io.to(socketId).emit("callUser", {
+  //     signal: signalData,
+  //     from: senderId,
+  //     name: sender_name,
+  //   });
+  // });
 
-  socket.on("accept_video_call", (data) => {
-    console.log("step 5");
-    const { senderId, receiverId, sender_name, receiver_name } = data;
-    const socketId = _userOnlines.get(receiverId);
-    socket.to(socketId).emit("connect_video_call");
-    console.log("step 6");
-    socket.on("answerCall", (signal) => {
-      console.log("step 7");
-      socket.to(socketId).emit("callAccepted", signal);
-    });
-  });
+  // socket.on("accept_video_call", (senderId) => {
+  //   console.log(senderId);
+  //   const socketId = _userOnlines.get(senderId);
+  //   io.to(socketId).emit("connect_video_call");
+  //   console.log("qua rooi ne");
+  // });
 
-  // when disconnect
-  socket.on("disconnect", (socket) => {
-    console.log("a user " + socket.id + " disconnected!");
-    io.emit("getUsers", _userOnlines);
-  });
+  // socket.on("answerCall", (data) => {
+  //   console.log(socketId);
+  //   const socketId = _userOnlines.get(data.to);
+  //   io.to(socketId).emit("callAccepted", data.signal);
+  // });
+
+  // socket.on("request_video_call", (data) => {
+  //   console.log("step 1");
+  //   const { senderId, receiverId, sender_name, receiver_name } = data;
+  //   //  if (!(_userCall.get(senderId) && _userCall.get(receiverId))) {
+  //   const socketId = _userOnlines.get(receiverId);
+  //   socket.to(socketId).emit("request_video_call", {
+  //     senderId: senderId,
+  //     sender_name: sender_name,
+  //     receiver_name: receiver_name,
+  //     receiverId: receiverId,
+  //   });
+  //   console.log("step 2");
+  //   socket.on("callUser", (signal) => {
+  //     console.log("step 3");
+  //     socket.to(socketId).emit("callUser", {
+  //       signal: signal,
+  //       senderId: senderId,
+  //       sender_name: sender_name,
+  //       receiver_name: receiver_name,
+  //       receiverId: receiverId,
+  //     });
+  //     console.log("step 4");
+  //   });
+  //   //    }
+  // });
+
+  // socket.on("accept_video_call", (data) => {
+  //   console.log("step 5");
+  //   const { senderId, receiverId, sender_name, receiver_name } = data;
+  //   const socketId = _userOnlines.get(receiverId);
+  //   socket.to(socketId).emit("connect_video_call");
+  //   console.log("step 6");
+  //   socket.on("answerCall", (signal) => {
+  //     console.log("step 7");
+  //     socket.to(socketId).emit("callAccepted", signal);
+  //   });
+  // });
+
+  // // when disconnect
+  // socket.on("disconnect", (socket) => {
+  //   console.log("a user " + socket.id + " disconnected!");
+  //   io.emit("getUsers", _userOnlines);
+  // });
 });
 
 module.exports = socketApi;
