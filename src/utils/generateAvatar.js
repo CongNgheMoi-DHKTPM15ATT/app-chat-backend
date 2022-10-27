ceba8236b6b2a18094479fd6c7407ccb721543f0
qswaquang <qswaquang@gmail.com>
2022-10-27 07:18:44 +0700

    avatar

--- src/utils/generateAvatar.js
+++ src/utils/generateAvatar.js
@@ -3,12 +3,13 @@ const { createCanvas } = require("canvas");
 exports.generateAvatar = function(user_name, foregroundColor, backgroundColor) {
   const canvas = createCanvas(200, 200)
   const context = canvas.getContext("2d");
+
   // Draw background
   context.fillStyle = backgroundColor;
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   // Draw text
-  context.font = "bold 100px";
+  context.font = "bold 100px Assistant";
   context.fillStyle = foregroundColor;
   context.textAlign = "center";
   context.textBaseline = "middle";
