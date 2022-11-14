08651cf7940a03061ba4d4cf2f80434a21d3043e
ANTi1302 <porkoe3443334@gmail.com>
2022-11-14 17:44:26 +0700

    create api mess by content type top 4

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -16,6 +16,17 @@ const messageController = {
     console.log(messages_document);
     return res.json(messages_document);
   }),
+  getAllByContentTypeTop4: asyncHandler(async (req, res) => {
+    const { conversation_id, content_type } = req.body;
+    const messages_document = await Message.find({
+      conversation_id: conversation_id,
+      content_type: content_type,
+    })
+      .limit(4)
+      .sort({ createdAt: -1 });
+    console.log(messages_document);
+    return res.json(messages_document);
+  }),
   deleteByConversation: asyncHandler(async (req, res) => {}),
   getMessageByConversation: asyncHandler(async (req, res, next) => {
     const { conversation_id, limit, offset } = req.body;
